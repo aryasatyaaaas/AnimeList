c38937343cabfd7e9b115e8cebf64e42570bc082
aryasatya <aryasatya@aryasatyas-MacBook-Pro.local>
2023-11-05 16:39:05 +0700

    animelist

---
+++ src/app/components/Search/index.js
@@ -0,0 +1,7 @@
+const Search = () => {
+    return(
+        <div>search components</div>
+    )
+}
+
+export default Search
---
+++ src/app/search/page.js
@@ -0,0 +1,10 @@
+const Page = () => {
+    return (
+        <>
+        <h1>search page</h1>
+        <p>ini adalah search page</p>
+        </>
+    )
+}
+
+export default Page
